68f7cdfb849cdb5720e7ac83079c20c5a3bdfccf
MichaelFoster <michaelrfoster0@gmail.com>
2020-09-30 11:54:04 -0400

    Implemented calculator functionality

--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -1,4 +1,9 @@
+package test.java;
+
 import org.junit.jupiter.api.Test;
+
+import main.java.Calculator;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestCalculator{
